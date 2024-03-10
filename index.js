f4cf710e02c2319e5bc16248ccc8e404a46f9c92
Wangguibin <864562082@qq.com>
2024-03-10 20:33:42 +0800

    改为降序

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 
-// 自定义比较函数
+// 自定义比较函数 降序
 function compareDates(a, b) {
-  var aDate = new Date(a.versionDate); // 转换成日期格式
+  var aDate = new Date(a.versionDate);  
   var bDate = new Date(b.versionDate);
-  if (aDate < bDate) return -1; // 返回小于0表示a应该排在前面
-  else if (aDate > bDate) return 1; // 返回大于0表示a应该排在后面
-  else return 0; // 相等则不变位置
+  if (aDate < bDate) return 1;  
+  else if (aDate > bDate) return -1;  
+  else return 0; 
 }
 
 try {
